4cdbdc63e182d339ae57e657d5ae63b17903f24a
Andrei <andrei.diaconu@studenti.unitn.it>
2018-12-05 12:04:24 +0100

    string_square done, now testing

--- string_square.js
+++ string_square.js
@@ -7,9 +7,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/square", (req, res) => {
+    if (Object.keys(req.query).length === 1) {
+        let s = req.query.string;
 
-    let square = string_square(req);
-    res.send(square);
+        if (typeof s === 'string' || s instanceof String) {
+            let square = string_square(s);
+            res.status(200);
+            res.send({ result: square });
+        } else {
+            res.status(400);
+            res.send({ result: -1 });
+        }
+    } else {
+        res.status(200);
+        res.end();
+    }
 });
 
 var port = process.env.PORT || 3000;
@@ -17,5 +29,7 @@ app.listen(port, () => console.log("server running on port " + port));
 
 
 function string_square(s) {
-    return "prova"
+    let square;
+    square = s.length * s.length;
+    return square;
 }
