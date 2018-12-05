2fc9de84012f9126d8398a06b8837ce4a46c7d97
Andrei <andrei.diaconu@studenti.unitn.it>
2018-12-05 11:45:45 +0100

    added server

--- string_square.js
+++ string_square.js
@@ -1,3 +1,21 @@
+var bodyParser = require('body-parser');
+
+// basic setup
+const app = require('express')();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/square", (req, res) => {
+
+    let square = string_square(req);
+    res.send(square);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log("server running on port " + port));
+
+
 function string_square(s) {
-    
+    return "prova"
 }
